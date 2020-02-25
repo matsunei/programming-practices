eb80f48252061389b8de148096c9530177aed38d
matsunei <54189313+matsunei@users.noreply.github.com>
2020-02-24 23:31:18 -0800

    Update Tennis.java

--- pp 7.2/Tennis.java
+++ pp 7.2/Tennis.java
@@ -1,7 +1,20 @@
 public class Tennis extends Sports
-{
-    public Tennis(String equipment, int playersPerTeam)
+{   
+    private boolean racketIsBig;
+    public Tennis(String equipment, int playersPerTeam, boolean racketIsBig)
     {
-    super(equipment, playersPerTeam);
+        super(equipment, playersPerTeam);
+        this.racketIsBig=racketIsBig;
     }
+    public boolean getRacketSize()
+    {
+        return racketIsBig;
+    }
+    public String toString()
+    {
+        String output="";
+        output += super.toString()+"\nTennis racket big?: "+racketIsBig;
+        return output;
+    }
+    
 }
