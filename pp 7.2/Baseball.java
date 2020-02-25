f12c38b6964b70f6d42390e3714a730ef20f4207
matsunei <54189313+matsunei@users.noreply.github.com>
2020-02-24 23:31:41 -0800

    Update Baseball.java

--- pp 7.2/Baseball.java
+++ pp 7.2/Baseball.java
@@ -1,7 +1,19 @@
 public class Baseball extends Sports
 {
-    public Baseball(String equipment, int playersPerTeam)
+    private int homeRunsScored;
+    public Baseball(String equipment, int playersPerTeam, int homeRunsScored)
     {
         super(equipment, playersPerTeam);
+        this.homeRunsScored = homeRunsScored;
+    }
+    public int getScore()
+    {
+        return homeRunsScored;
+    }
+    public String toString()
+    {
+        String output="";
+        output += super.toString()+"\nHome runs scored: "+homeRunsScored;
+        return output;
     }
 }
